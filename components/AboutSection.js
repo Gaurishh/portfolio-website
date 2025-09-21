27019d51163e125cab687fa69e2ce35b1edd40a1
Gaurish <9gaurish@gmail.com>
2025-09-22 01:23:02 +0530

    About me zoom out amount changed

--- components/AboutSection.js
+++ components/AboutSection.js
@@ -13,8 +13,8 @@ const AboutSection = () => (
                      alt="Your Name - Software Engineer" 
                      width={512}
                      height={512}
-                      className="rounded-full w-64 h-64 xl:w-[28rem] xl:h-[28rem] border-4 border-gray-700 object-cover object-top"
-                     style={{ objectPosition: 'center 30%' }}
+                     className="rounded-full w-64 h-64 xl:w-[28rem] xl:h-[28rem] border-4 border-gray-700 object-cover"
+                    style={{ objectPosition: 'center 50%' }}
                      priority
                  />
              </div>
